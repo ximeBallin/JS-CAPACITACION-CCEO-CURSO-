701afdefe00d242a5dbd2455b9a145bd1e28991f
Xime <ximena.ruiz@cceo.com.mx>
2026-08-28 11:16:29 -0600

    feat (01-Introduccion):✨Section 01 of the course has been completed

---
+++ 01-Introduccion/JS/app.js
@@ -0,0 +1,11 @@
+
+//Pregunta al usuario su nombre
+const nombre = prompt('¿Cuál es tu nombre?');
+
+//Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScript moderno con Juan`;
+
+//Asigna el valor hacia la variable producto
+const producto = 'Monitor 24 pulgadas';
+
+
