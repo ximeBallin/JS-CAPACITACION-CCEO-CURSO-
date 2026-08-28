862493b2d52146c1ce57132addbfa9c6f1dc3a46
Xime <ximena.ruiz@cceo.com.mx>
2026-08-28 12:16:15 -0600

    feat (02-Variables): ✨ Section 02 variables in JS completed

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,44 @@
+//Incializar una variable con un valor
+var productos = "Monitor 24 Pulgadas";
+console-log(producto);
+
+//Las variables se pueden asignar
+productos = "Monitor 19 Pulgadas";
+console.log(producto);
+
+//JS es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto=20;
+console.log(producto);
+
+//Se pueden incializar sin valor y asignarlo despues
+var disponibles;
+disponibles = true;
+
+disponibles = false;
+
+//Incializar multiples variables
+var categoria = "Computadoras",
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+
+/*
+    var 99dias;
+    var dias99;
+
+    var 01_;
+    var _01;
+    var _usuario;
+    var __usuario;
+
+    var nombreProducto;
+    var nombre_producto_categoria_precio;
+    var nombre producto 
+*/
+
+
+
+
+
+
+
---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,17 @@
+let producto = "Tablet";
+
+// Reasignar el valor
+producto="Monitor";
+prodycto = 20;
+producto = true;
+producto = null;
+
+console.log(producto);
+
+let precio;
+
+precio = 300;
+
+console.log(precio);
+
+
