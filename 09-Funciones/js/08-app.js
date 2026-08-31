7f8d97c480a75dd96bef233ab278c39ab8ae380a
Xime <ximena.ruiz@cceo.com.mx>
2026-08-31 10:26:56 -0600

    feat(09-Functions-JS): ✨ Section 09 Functions in JS has been completed

---
+++ 09-Funciones/js/08-app.js
@@ -0,0 +1,18 @@
+iniciarApp();
+
+function iniciarApp() {
+    console.log('Iniciando app...');
+
+    segundaFuncion();
+}
+
+function segundaFuncion() {
+    console.log('Desde la segunda función');
+
+    usuarioAutenticado('Pablo');
+}
+
+function usuarioAutenticado(usuario) {
+    console.log('Autenticando usuario... espere...');
+    console.log(`Usuario autenticado exitosamente: ${usuario} `);
+}
---
+++ 09-Funciones/js/11-app.js
@@ -0,0 +1,8 @@
+const aprendiendo = function(tecnologia) {
+    console.log(`Aprendiendo ${tecnologia}`);
+}
+aprendiendo('JavaScript')
+
+const aprendiendo2 = (tecnologia) => 'Aprendiendo JavaScript';
+
+console.log(aprendiendo2());
