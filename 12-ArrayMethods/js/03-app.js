13485da5365ceb769e9b0d2dece576dc1442dc90
Xime <ximena.ruiz@cceo.com.mx>
2026-08-31 13:33:35 -0600

    feat(12-ArrayMethods-JS): ✨ complete section 12 array methods in javascript

---
+++ 12-ArrayMethods/js/03-app.js
@@ -0,0 +1,17 @@
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 100 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+]
+
+// con un foreach
+let total = 0;
+carrito.forEach( producto => total += producto.precio );
+console.log(total);
+
+// Con un reduce
+let resultado = carrito.reduce( (total, producto) => total + producto.precio, 0);
+console.log(resultado);
