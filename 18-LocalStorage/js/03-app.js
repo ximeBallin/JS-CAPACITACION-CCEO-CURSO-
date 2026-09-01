e8e8ba2abdf8eb1969862929e74b7e9d1611fbe4
Xime <ximena.ruiz@cceo.com.mx>
2026-09-01 09:51:15 -0600

    feat (18-LocalStorage-JS):✨The LocalStorage section in JS  has been completed

---
+++ 18-LocalStorage/js/03-app.js
@@ -0,0 +1,10 @@
+localStorage.setItem('nombre');
+
+//Actualizar un registro 
+const mesesArray =JSON.parse(localStorage.getItem('meses'));
+console.log(mesesArray);
+mesesArray.push('Nuevo Mes');
+console.log(mesesArray);
+localStorage.setItem('meses', JSON.stringify(mesesArray));  
+
+localStorage.clear();
