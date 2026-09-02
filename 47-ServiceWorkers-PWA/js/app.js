ab1796a15b77cd2eafe8c6d18cc16fc49a167de9
Xime <ximena.ruiz@cceo.com.mx>
2026-09-02 12:56:34 -0600

    feat (PWA): ✨implement service worker registration, lifecycle events, and web app manifest configuration

---
+++ 47-ServiceWorkers-PWA/js/app.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator){
+    navigator.serviceWorker.register('./sw.js')
+        .then(registrado => console.log('Se instalo correctamente...', registrado))
+        .catch(error => console.log('Fallo la instalacion...', error));
+
+}else{
+    console.log('service workers no soportados');
+}
